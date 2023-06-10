d390da993ecb73b2ac5b04940e96341a1c2c5137
Devansh Patil <devanshpatil@Devanshs-MacBook-Pro.local>
2023-06-10 07:00:32 -0700

    commit #1

---
+++ swappingTwoNumbers.py
@@ -0,0 +1,5 @@
+#ask for the first digit of the num
+#ask for the 2nd
+add the two nums
+subtract the 1st num from the sum and store as the first digit
+subtract the 2nd num from the sun and store as the second digit
